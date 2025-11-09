ef0c2772aaccddb9ddf6a40fbb0ab57c027ac870
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-11-09 22:58:03 +0100

    improved base app

--- src/main.py
+++ src/main.py
@@ -1,22 +1,35 @@
 import ttkbootstrap as ttk
-from ttkbootstrap.constants import *
 from ddgs import DDGS
+from ttkbootstrap.constants import *
 
 root = ttk.Window(themename="darkly")
 
-b1 = ttk.Button(root, text="Button 1", bootstyle=WARNING)
-b1.pack(side=LEFT, padx=5, pady=10)
+root.geometry("1600x900")
+root.title("WebSearch++")
 
-b2 = ttk.Button(root, text="Button 2", bootstyle=(INFO, OUTLINE))
-b2.pack(side=LEFT, padx=5, pady=10)
+entry_frame = ttk.Frame(root)
+entry_frame.pack(side=TOP, expand=True, fill=X, padx=80, pady=80)
+
+search_bar = ttk.Entry(master=entry_frame,textvariable="search_request")
+search_bar.insert(END, "Search")
+search_bar.pack(side=LEFT,expand=True, fill=X)
+
+b1 = ttk.Button(entry_frame, text="Search", bootstyle=PRIMARY)
+b1.pack(side=RIGHT, expand=False, padx=5, pady=10)
 
 if __name__ == "__main__":
     # EXEMPLE
-    results = DDGS().text('russia filetype:pdf', region='us-en', safesearch='off', timelimit='y', page=1,
-                          backend="auto")
+    results = DDGS().text(
+        "russia filetype:pdf",
+        region="us-en",
+        safesearch="off",
+        timelimit="y",
+        page=1,
+        backend="auto",
+    )
     for r in results:
-        print(r['title'])
-        print(r['href'])
+        print(r["title"])
+        print(r["href"])
         print()
 
     root.mainloop()
