6c91819b3b22cac48b6abbf576fdaa1c316e64d4
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-12-08 11:13:24 +0100

    feat: update previous page button to use a click handler instead of href

--- vue/components/previous_page_button.py
+++ vue/components/previous_page_button.py
@@ -1,14 +1,15 @@
 """A component to display a back button."""
 import reflex as rx
 from vue.states.history_state import HistoryState, HistoryEntry
+from typing import Callable
 
 
-def previous_page_button() -> rx.Component:
+def previous_page_button(on_click_handler: Callable) -> rx.Component:
     """Render a single back button."""
-    return rx.link(
+    return rx.button(
                 rx.icon("arrow-left", size=16),
                 "Back",
-                href="/",
                 color_scheme="gray",
                 margin_bottom="16px",
+                on_click=on_click_handler,
             )
